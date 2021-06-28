402aee17416b3bf5f6bf3c62739e2eb710f3a828
Ryuk Tan <ryuk.tan@software.acer.net>
2021-06-28 08:44:20 +0800

    Add project files.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace AzureKeyVaultLab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var value = _configuration["test"];
+            return $"Value for secret [test] is : {value}";
+        }
+    }
+}
